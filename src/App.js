7f342be708dfded12a79da29e0651de30a84709d
S.GordonShryock <sgshryock@outlook.com>
2022-01-24 21:07:59 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there my dude! How are you?
         </p>
         <a
           className="App-link"
